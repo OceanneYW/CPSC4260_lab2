5da53ca308e38a87ff3e0036d8c648fbe2b51715
Oceanne Wang <oceanneywang@gmail.com>
2023-01-24 12:00:27 -0800

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Load data from CSV file asynchronously and visualize it
+ */
+d3.csv('data/rawdata.csv')
+  .then(data => {
+    
+    // Todo: Preprocess data and show chart
+    
+  })
+  .catch(error => console.error(error));
